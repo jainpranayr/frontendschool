2a1e9019e7d431d1e6ce8e102a873e961d0c017c
Pranay Jain <jpranay688@gmail.com>
2021-08-20 02:46:58 +0530

    add footer

---
+++ script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibGVvcGFyZGxlbnMiLCJhIjoiY2tzamIwc3JyMGh3bDJ2bmN5dzlkc2wzcSJ9.oGqroZO-JyBVthS5CjuHvA";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-71.060982, 42.35725],
+  zoom: 18,
+});
